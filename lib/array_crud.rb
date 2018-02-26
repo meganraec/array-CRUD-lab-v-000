ded97bf7129336b049c9e26ba1fad231d867d305
Megan Collier <meganraec@live.com>
2018-02-26 21:21:08 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,23 +19,26 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
-end
-
-def remove_element_from_start_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
   array.pop
 end
 
-def retrieve_element_from_index(array, index_number)
+def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.shift
 end
 
-def retrieve_first_element_from_array(array)
+def retrieve_element_from_index(array, index_number)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  return array[2]
+end
 
+def retrieve_first_element_from_array(array)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.last
 end
